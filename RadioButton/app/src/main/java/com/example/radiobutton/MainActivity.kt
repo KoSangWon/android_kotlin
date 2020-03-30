fa07077e62b45bf1ffec99c14979e999b22f6d9f
KoSangWon <jackoss@naver.com>
2020-03-30 17:29:15 +0900

    RadioButton

---
+++ RadioButton/app/src/main/java/com/example/radiobutton/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.radiobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun init(){
+        imgGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.radioButton1 -> imageView.setImageResource((R.drawable.img1))
+                R.id.radioButton2 -> imageView.setImageResource((R.drawable.img2))
+                R.id.radioButton3 -> imageView.setImageResource((R.drawable.img3))
+            }
+        }
+    }
+}
