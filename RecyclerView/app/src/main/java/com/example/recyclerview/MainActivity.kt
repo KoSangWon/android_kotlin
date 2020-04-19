37ae2553c230fdd56d94f147d4804eebe14af654
KoSangWon <jackoss@naver.com>
2020-04-19 12:31:35 +0900

    menubar 추가

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,26 @@ class MainActivity : AppCompatActivity() {
         initRecyclerView()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu1, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menuitem1 -> {
+                recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            }
+            R.id.menuitem2 -> {
+                recyclerView.layoutManager = GridLayoutManager(this, 3)
+            }
+            R.id.menuitem3 -> {
+                recyclerView.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun initRecyclerView() {
         recyclerView.layoutManager = LinearLayoutManager(this,
         LinearLayoutManager.VERTICAL, false)
@@ -23,14 +46,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun initData(){
         data.add(MyData("item1", 10))
-        data.add(MyData("item2", 20))
+        data.add(MyData("item2", 15))
         data.add(MyData("item3", 15))
-        data.add(MyData("item4", 30))
-        data.add(MyData("item5", 25))
-        data.add(MyData("item6", 10))
-        data.add(MyData("item7", 20))
-        data.add(MyData("item8", 15))
-        data.add(MyData("item9", 40))
-        data.add(MyData("item10", 35))
+        data.add(MyData("item4", 10))
+        data.add(MyData("item5", 20))
+        data.add(MyData("item6", 15))
+        data.add(MyData("item7", 10))
+        data.add(MyData("item8", 20))
+        data.add(MyData("item9", 15))
+        data.add(MyData("item10", 20))
     }
 }
