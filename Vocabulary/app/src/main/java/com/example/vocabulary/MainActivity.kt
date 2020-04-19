8ec7ac7a1834f8198b0cef506b951e87db0f4073
KoSangWon <jackoss@naver.com>
2020-04-19 14:02:09 +0900

    event처리 전

--- Vocabulary/app/src/main/java/com/example/vocabulary/MainActivity.kt
+++ Vocabulary/app/src/main/java/com/example/vocabulary/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.vocabulary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    var words = mutableMapOf<String, String>()
+    var array = ArrayList<String>()
+    lateinit var adapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
+
+    private fun init() {
+        readFile()
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = MyAdapter(array)
+        recyclerView.adapter = adapter
+    }
+
+    fun readFile(){
+        val scan = Scanner(resources.openRawResource(R.raw.words))
+        while(scan.hasNextLine()){
+            val word = scan.nextLine()
+            val meaning = scan.nextLine()
+            words[word] = meaning
+            array.add(word)
+        }
+        scan.close()
+    }
+
 }
---
+++ Vocabulary/app/src/main/java/com/example/vocabulary/MyAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.vocabulary
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MyAdapter(val items: ArrayList<String>)
+    :RecyclerView.Adapter<MyAdapter.MyViewHolder>()
+{
+
+    
+
+    inner class MyViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+        var textView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+        return MyViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+}
