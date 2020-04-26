fa64d3a32c5980724991eac9d34a291122567764
KoSangWon <jackoss@naver.com>
2020-04-27 01:47:13 +0900

    final

--- VocaQuiz/app/src/main/java/com/example/vocaquiz/MainActivity.kt
+++ VocaQuiz/app/src/main/java/com/example/vocaquiz/MainActivity.kt
@@ -11,13 +11,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.row.view.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     var words = mutableMapOf<String, String>()
     var englishArray = ArrayList<String>()
     var meaningArray = ArrayList<String>()
 
-    //var score = intent.getIntExtra("SCORE", 0)
+
 
     lateinit var adapter:MyAdapter
 
@@ -28,7 +29,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init(){
-        //scoreText.text = score.toString()
+        var score = intent.getIntExtra("SCORE", 0)
+        var solvedCnt = intent.getIntExtra("SOLVED", 0)
+        var correctCnt = intent.getIntExtra("CORRECT", 0)
+        var tempArr = ArrayList<Int>()
+        var flag = false
+
+        scoreText.text = "점수 : $score"
+        solved.text = "푼 문제 수 : $solvedCnt"
+        correct.text = "맞은 문제 수 : $correctCnt"
+
         readFile()
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
@@ -46,13 +56,29 @@ class MainActivity : AppCompatActivity() {
                 data: String,
                 position: Int
             ) {
-                if(words[english.text.toString()]?.equals(holder.voca.text)!!){
-                    Toast.makeText(applicationContext, "정답입니다!", Toast.LENGTH_SHORT).show()
-                    //score += 10
-                }else{
+                for(i in 0 until englishArray.size){
+                    if(english.text == englishArray[i]) {
+                        tempArr.add(i)
+                    }
+                }
+
+                for(i in 0 until tempArr.size){
+                    if(holder.voca.text == meaningArray[tempArr[i]]) {
+                        Toast.makeText(applicationContext, "정답입니다!", Toast.LENGTH_SHORT).show()
+                        score += 10
+                        correctCnt++
+                        flag = true
+                        break
+                    }
+                }
+                if(!flag){
                     Toast.makeText(applicationContext, "오답입니다!", Toast.LENGTH_SHORT).show()
                 }
-                //intent.putExtra("SCORE", score)
+                solvedCnt++
+
+                intent.putExtra("SCORE", score)
+                intent.putExtra("SOLVED", solvedCnt)
+                intent.putExtra("CORRECT", correctCnt)
                 finish()
                 startActivity(intent)
             }
