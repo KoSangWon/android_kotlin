29baca2f0989f3d2f7cab4cbf88bc7b0821ba992
KoSangWon <jackoss@naver.com>
2020-04-27 22:12:32 +0900

    implicitIntent

---
+++ ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.implicitintent
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val CALL_REQUEST = 100
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun callAction(){
+        val number = Uri.parse("tel:010-3477-8476")
+        val callIntent = Intent(Intent.ACTION_CALL, number)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), CALL_REQUEST)
+            //return
+        }
+        else {
+            startActivity(callIntent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CALL_REQUEST -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "권한이 승인되었습니다.", Toast.LENGTH_SHORT).show()
+                    callAction()//다시 실행
+                }
+                else{
+                    Toast.makeText(this, "권한 승인이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    private fun init(){
+        callbtn.setOnClickListener {
+            callAction()
+        }
+
+        msgbtn.setOnClickListener {
+            val message = Uri.parse("sms:010-3477-8476")
+            val messageIntent = Intent(Intent.ACTION_SENDTO, message)
+            messageIntent.putExtra("sms_body", "빨리 다음꺼 하자..")
+            startActivity(messageIntent)
+        }
+
+        webbtn.setOnClickListener {
+            val webpage = Uri.parse("http://www.naver.com")
+            val webIntent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(webIntent)
+        }
+
+        mapbtn.setOnClickListener {
+            val location = Uri.parse("geo:37.543684,127.077130?z=16")
+            val mapIntent = Intent(Intent.ACTION_VIEW, location)
+            startActivity(mapIntent)
+        }
+    }
+}
