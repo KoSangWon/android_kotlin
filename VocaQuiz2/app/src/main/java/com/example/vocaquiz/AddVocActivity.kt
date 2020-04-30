573295e7ba16a0209fc3fa7e73bd9daac8780738
KoSangWon <jackoss@naver.com>
2020-05-01 00:20:58 +0900

    VocaAddActivity

--- VocaQuiz2/app/src/main/java/com/example/vocaquiz/AddVocActivity.kt
+++ VocaQuiz2/app/src/main/java/com/example/vocaquiz/AddVocActivity.kt
@@ -1,12 +1,42 @@
 package com.example.vocaquiz
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_voc.*
+import java.io.PrintStream
 
 class AddVocActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_voc)
+        init()
+    }
+
+    private fun init(){
+        addBtn.setOnClickListener {
+            val word = editWord.text.toString()
+            val meaning = editMeaning.text.toString()
+            writeFile(word, meaning)
+        }
+
+        cancelBtn.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+    }
+
+    private fun writeFile(word: String, meaning: String) {
+        val output = PrintStream(openFileOutput("out.txt", Context.MODE_APPEND))
+        output.println(word)
+        output.println(meaning)
+        output.close()
+        val i = Intent()
+        i.putExtra("voc", MyData(word, meaning))
+        setResult(Activity.RESULT_OK, i)
+        finish()
     }
 }
