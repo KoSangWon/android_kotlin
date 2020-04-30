4cd77a314d05c99f5307c00b6d33a9301ef934a8
KoSangWon <jackoss@naver.com>
2020-05-01 00:13:21 +0900

    IntroActivity

---
+++ VocaQuiz2/app/src/main/java/com/example/vocaquiz/AddVocActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vocaquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddVocActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_voc)
+    }
+}
