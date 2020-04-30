58cd4d00888d8ba33c55905e86c5190cfdefe1e7
KoSangWon <jackoss@naver.com>
2020-05-01 00:01:08 +0900

    setting

---
+++ VocaQuiz2/app/src/main/java/com/example/vocaquiz/MyAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.vocaquiz
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
+import java.util.*
+
+class MyAdapter(val items: ArrayList<String>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    interface OnItemClickListener{
+        fun OnItemClick(holder: MyViewHolder, view:View, data:String, position: Int)
+    }
+
+    var itemClickListener:OnItemClickListener?=null
+    var exampleArray = ArrayList<Int>()
+
+    inner class MyViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        var voca:TextView = itemView.findViewById(R.id.voca)
+
+        init{
+            itemView.setOnClickListener {
+                itemClickListener?.OnItemClick(this, it, items[adapterPosition], adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+        return MyViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val len = items.size
+
+        while(exampleArray.size < 5){
+            val num = Random().nextInt(len)
+            if(!exampleArray.contains(num)) {
+                exampleArray.add(num)
+                exampleArray = exampleArray.shuffled() as ArrayList<Int>
+            }
+        }
+
+        holder.voca.text = items[exampleArray[position]]
+    }
+
+
+}
