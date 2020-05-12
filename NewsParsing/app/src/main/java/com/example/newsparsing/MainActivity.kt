6b0d41fbf9ae89a633a32c431449cd64c28e2134
Sangwon <jackoss@naver.com>
2020-05-13 02:26:56 +0900

    RecyclerView Setting & jsoup

--- NewsParsing/app/src/main/java/com/example/newsparsing/MainActivity.kt
+++ NewsParsing/app/src/main/java/com/example/newsparsing/MainActivity.kt
@@ -14,13 +14,7 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    var words = mutableMapOf<String, String>()
-    var englishArray = ArrayList<String>()
-    var meaningArray = ArrayList<String>()
-
-
-
-    lateinit var adapter:MyAdapter
+    lateinit var adapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,62 +22,8 @@ class MainActivity : AppCompatActivity() {
         init()
     }
 
-    private fun init(){
-        var score = intent.getIntExtra("SCORE", 0)
-        var solvedCnt = intent.getIntExtra("SOLVED", 0)
-        var correctCnt = intent.getIntExtra("CORRECT", 0)
-        var tempArr = ArrayList<Int>()
-        var flag = false
-
-        scoreText.text = "점수 : $score"
-        solved.text = "푼 문제 수 : $solvedCnt"
-        correct.text = "맞은 문제 수 : $correctCnt"
-
-
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
-        adapter = MyAdapter(meaningArray)
-        recyclerView.adapter = adapter
-
-        var randNum = Random().nextInt(englishArray.size)
-        english.text = englishArray[randNum]
-        adapter.exampleArray.add(randNum)
-
-        adapter.itemClickListener = object:MyAdapter.OnItemClickListener{
-            override fun OnItemClick(
-                    holder: MyAdapter.MyViewHolder,
-                    view: View,
-                    data: String,
-                    position: Int
-            ) {
-                for(i in 0 until englishArray.size){
-                    if(english.text == englishArray[i]) {
-                        tempArr.add(i)
-                    }
-                }
-
-                for(i in 0 until tempArr.size){
-                    if(holder.voca.text == meaningArray[tempArr[i]]) {
-                        Toast.makeText(applicationContext, "정답입니다!", Toast.LENGTH_SHORT).show()
-                        score += 10
-                        correctCnt++
-                        flag = true
-                        break
-                    }
-                }
-                if(!flag){
-                    Toast.makeText(applicationContext, "오답입니다!", Toast.LENGTH_SHORT).show()
-                }
-                solvedCnt++
-
-                intent.putExtra("SCORE", score)
-                intent.putExtra("SOLVED", solvedCnt)
-                intent.putExtra("CORRECT", correctCnt)
-                finish()
-                startActivity(intent)
-            }
-        }
-
+    fun init() {
+        
     }
 
 }
