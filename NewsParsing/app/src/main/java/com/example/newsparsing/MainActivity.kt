fc1945dc3e001c331ff060cc0a136df254e97e25
Sangwon <jackoss@naver.com>
2020-05-13 11:35:19 +0900

    add swiperefreshlayout

--- NewsParsing/app/src/main/java/com/example/newsparsing/MainActivity.kt
+++ NewsParsing/app/src/main/java/com/example/newsparsing/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.newsparsing
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
+import android.icu.lang.UCharacter
 import android.net.Uri
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.core.view.get
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.row.view.*
+import org.jsoup.Jsoup
+import java.lang.ref.WeakReference
+import java.net.URL
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -23,10 +29,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+        startTask()
+    }
+
+    fun startTask(){
+        val task = MyAsyncTask(this)
+        task.execute(URL("https://www.daum.net"))
     }
 
     fun init() {
+
+        swiperefresh.setOnRefreshListener {
+            swiperefresh.isRefreshing = true
+            startTask()
+        }
+
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         adapter = MyAdapter(ArrayList<MyData>())
         adapter.itemClickListener = object:MyAdapter.OnItemClickListener{
             override fun OnItemClick(
@@ -42,4 +61,27 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
     }
 
+    class MyAsyncTask(context: MainActivity):AsyncTask<URL, Unit, Unit>(){
+        val activityreference = WeakReference(context)
+
+        override fun doInBackground(vararg params: URL?): Unit {
+            val activity = activityreference.get()
+            activity?.adapter?.items?.clear()
+            val doc = Jsoup.connect(params[0].toString()).get()
+            val headlines = doc.select("ul.list_txt>li>a")
+            for(news in headlines){
+                activity?.adapter?.items?.add(MyData(news.text(), news.absUrl("href")))
+            }
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+            val activity = activityreference.get()
+            if(activity==null || activity.isFinishing){
+                return
+            }
+            activity.swiperefresh.isRefreshing = false
+            activity.adapter.notifyDataSetChanged()
+        }
+    }
 }
