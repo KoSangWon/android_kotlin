24646009db85e807b332285060bb333ae61afbb3
Sangwon <jackoss@naver.com>
2020-05-15 22:07:09 +0900

    add fragment

--- Layout/app/src/main/java/com/example/layout/MainActivity.kt
+++ Layout/app/src/main/java/com/example/layout/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val checkbox = mutableListOf<CheckBox>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
+
+    fun init(){
+        checkbox.add(checkBox1)//checkbox.add(findViewById(R.id.checkBox1))해도 됨
+        checkbox.add(checkBox2)
+        checkbox.add(checkBox3)
+        checkbox.add(checkBox4)
+        checkbox.add(checkBox5)
+        checkbox.add(checkBox6)
+        checkbox.add(checkBox7)
+        checkbox.add(checkBox8)
+        checkbox.add(checkBox9)
+        checkbox.add(checkBox10)
+
+        for(check in checkbox){
+            check.setOnCheckedChangeListener { buttonView, isChecked ->
+                val imgID = resources.getIdentifier(buttonView.text.toString(), "id", packageName)
+                val imgView = findViewById<ImageView>(imgID)
+
+                if(isChecked)
+                    imgView.visibility = View.VISIBLE
+                else
+                    imgView.visibility = View.INVISIBLE
+            }
+        }
+        }
 }
--- test/app/src/main/java/com/example/test/MainActivity.kt
+++ test/app/src/main/java/com/example/test/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 
 class MainActivity : AppCompatActivity() {
+
+    var words = mutableMapOf<String, String>()
+    var array = ArrayList<String>()
+    lateinit var adapter:MyAdapter
+    lateinit var tts: TextToSpeech
+    var isTtsReady = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
 }
+
