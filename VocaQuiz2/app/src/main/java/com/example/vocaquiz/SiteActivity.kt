4c27959366b0ee0428d0693748b43d6f7f10fb6a
Sangwon <jackoss@naver.com>
2020-05-16 22:06:48 +0900

    add siteActivity

---
+++ VocaQuiz2/app/src/main/java/com/example/vocaquiz/SiteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vocaquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_site)
+    }
+}
